206fc1ba8a588b3eed5d4b8a076de4210d640a70
Ranesh Jangid <nenobrain.io@gmail.com>
2026-09-19 08:56:18 +0530

    Add transparent Neev logo and place before emblem in Header

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import emblem from '../../assets/Emblem_of_India.svg';
+import neevLogo from '../../assets/neev.png';
 import './Header.css';
 
 export default function Header() {
   return (
     <header className="custom-top-header">
       <div className="header-left">
+        <img src={neevLogo} alt="Neev" className="header-logo neev-logo" />
         <img src={emblem} alt="भारत का राजचिह्न" className="header-logo" />
         <h1 className="header-title">रविन्द्र सिंह भाटी</h1>
       </div>
