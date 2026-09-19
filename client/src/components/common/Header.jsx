1385605985dfc32cb2a692687feacc635118ef57
Ranesh Jangid <nenobrain.io@gmail.com>
2026-09-19 08:59:19 +0530

    Move Neev logo to the right side of the header

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -7,9 +7,11 @@ export default function Header() {
   return (
     <header className="custom-top-header">
       <div className="header-left">
-        <img src={neevLogo} alt="Neev" className="header-logo neev-logo" />
         <img src={emblem} alt="भारत का राजचिह्न" className="header-logo" />
         <h1 className="header-title">रविन्द्र सिंह भाटी</h1>
+      </div>
+      <div className="header-right">
+        <img src={neevLogo} alt="Neev" className="header-logo neev-logo" />
       </div>
     </header>
   );
